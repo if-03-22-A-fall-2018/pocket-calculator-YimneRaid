16f3e63fef291ebaef053b8ac86b1fb3ad7739b7
Yimne Raid <yimne.raid@outlook.com>
2018-10-02 11:36:05 +0200

    Error and warning free

--- pocket_calcolator.c
+++ pocket_calcolator.c
@@ -3,36 +3,6 @@ void Print (){
   printf("Choose one of the following operations:\nAdd (1)\nSubtract (2)\nMultiply (3)\nDivide (4)\nStop program (-1)\nEnter your choice: ");
 }
 
-int Choice(double firstNumber,double secondNumber){
-  int choise;
-  switch(choise){
-      case 1:Add(firstNumber,secondNumber);break;
-      case 2:Subtract(firstNumber,secondNumber);break;
-      case 3:Multiply(firstNumber,secondNumber);break;
-      case 4:while(secondNumber==0){
-        printf("Error! Please enter another number to divede: \n" );
-        secondNumber=EnterSecondNumber();
-        if (secondNumber!=0){
-          Divide(firstNumber,secondNumber);break;
-        }
-      }
-  }
-}
-
-double EnterFirstNumber(){
-  double firstNumber;
-  printf("Please enter the first operand:" );
-  scanf("%f", &firstNumber);
-  return firstNumber;
-}
-
-double EnterSecondNumber(){
-  double secondNumber;
-  printf("Please enter the second operand:" );
-  scanf("%f", &secondNumber);
-  return secondNumber;
-}
-
 double Add(double firstNumber, double secondNumber){
   double solution;
   solution=firstNumber+secondNumber;
@@ -41,7 +11,7 @@ double Add(double firstNumber, double secondNumber){
 
 double Subtract(double firstNumber, double secondNumber){
   double solution;
-  soolution = firstNumber-secondNumber;
+  solution = firstNumber-secondNumber;
   return solution;
 
 
@@ -63,12 +33,41 @@ double Divide(double firstNumber, double secondNumber){
   return solution;
 }
 
+double EnterSecondNumber(){
+  double secondNumber;
+  printf("Please enter the second operand:" );
+  scanf("%lf", &secondNumber);
+  return secondNumber;
+}
+
+int Choice(double firstNumber,double secondNumber){
+  int choise;
+  switch(choise){
+      case 1:Add(firstNumber,secondNumber);break;
+      case 2:Subtract(firstNumber,secondNumber);break;
+      case 3:Multiply(firstNumber,secondNumber);break;
+      case 4:while(secondNumber==0){
+        printf("Error! Please enter another number to divede: \n" );
+        secondNumber=EnterSecondNumber();
+        if (secondNumber!=0){
+          Divide(firstNumber,secondNumber);break;
+        }
+      }
+  }
+}
+
+double EnterFirstNumber(){
+  double firstNumber;
+  printf("Please enter the first operand:" );
+  scanf("%lf", &firstNumber);
+  return firstNumber;
+}
 
  int main(int argc, char const *argv[]) {
    Print();
    double firstOperand=EnterFirstNumber();
    double secondOperand=EnterSecondNumber();
    double result=Choice(firstOperand,secondOperand);
-   printf("Result=",result );
+   printf("Result=%lf",result );
   return 0;
 }
